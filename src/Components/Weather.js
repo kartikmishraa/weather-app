7c7deda3ba11f742fb8ef3ee368eea39047a50ca
Kartik Mishra <kartikm2219@gmail.com>
2022-06-26 02:14:39 +0530

    refactored code

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -11,51 +11,29 @@ export default function Weather(props) {
     return num.toFixed(2);
   }
 
-  const place = props.weatherData.name;
-  const country = props.weatherData.sys.country;
-  const maxTemp = formatTemp(props.weatherData.main.temp_max);
-  const minTemp = formatTemp(props.weatherData.main.temp_min);
-  const feelsLike = formatTemp(props.weatherData.main.feels_like);
+  const data = {
+    place: props.weatherData.name,
+    country: props.weatherData.sys.country,
+    maxTemp: formatTemp(props.weatherData.main.temp_max),
+    minTemp: formatTemp(props.weatherData.main.temp_min),
+    feelsLike: formatTemp(props.weatherData.main.feels_like),
+  };
 
   return (
-    // <>
-    //   {/* <div>Weather</div> */}
-    //   <div className="card text-only">
-    //     <div className="card-title">
-    //       <div>
-    //         <h3>{place}</h3>
-    //         <h5 className="card-description">{country}</h5>
-    //       </div>
-    //     </div>
-    //     <div className="card-info">
-    //       <p>
-    //         <i>{moment().format("dddd")}</i>
-    //         <br />
-    //         <i>{moment().format("LL")}</i>
-    //         <br />
-    //         <br />
-    //         Max. Temp: {maxTemp} &deg;C
-    //         <br />
-    //         Min. Temp: {minTemp} &deg;C
-    //         <br />
-    //         Feels Like: {feelsLike} &deg;C
-    //       </p>
-    //     </div>
-    //   </div>
-    // </>
     <Card className="text-center">
-      <Card.Header>{place}</Card.Header>
+      <Card.Header>{data.place}</Card.Header>
       <Card.Body>
         <Card.Title>
           {moment().format("dddd")}, {moment().format("LL")}
         </Card.Title>
         <Card.Text>
-          Max. Temp: {maxTemp} &deg;C <br /> Min. Temp: {minTemp} &deg;C <br />
-          Feels Like: {feelsLike} &deg;C
+          Max. Temp: {data.maxTemp} &deg;C <br /> Min. Temp: {data.minTemp}{" "}
+          &deg;C <br />
+          Feels Like: {data.feelsLike} &deg;C
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
-      <Card.Footer className="text-muted">{country}</Card.Footer>
+      <Card.Footer className="text-muted">{data.country}</Card.Footer>
     </Card>
   );
 }
